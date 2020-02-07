8e333bb4d2b0afd0c094801fbd061a0c6867ae28
Serghei <seryga_ms@mail.ru>
2020-02-07 11:11:24 +0200

    Added Bootstrap module

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,10 +1,27 @@
+<?php $this->beginPage(); ?>
 <html>
 <head>
     <title>Video School</title>
+        <?php $this->head();?>
 </head>
-    <body>
+<body>
+    <?php $this->beginBody();?>
+            <?php
+                use \yii\bootstrap\NavBar;
 
-        <?= $content ?>
-
-    </body>
+                    NavBar::begin([
+                        'brandLabel' => 'ITVDN',
+                        'brandUrl' => 'Yii'::$app->homeUrl,
+                        'options' => [
+                            'class' => 'navbar-default navbar-fixed-top'
+                        ]
+                    ]);            
+                    NavBar::end();
+            ?>
+        <div class="container" style="margin-top: 60px">
+            <?= $content ?>
+        </div>
+        <?php $this->endBody();?>
+</body>
 </html>
+<?php $this->endPage();?>
